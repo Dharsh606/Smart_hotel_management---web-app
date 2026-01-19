8da693b4f73dc8e578c991bd9369b8fedc12ca5d
Dharsh606 <dharshan.vreddy2819@gmail.com>
2026-01-19 16:09:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -559,4 +559,5 @@ if __name__ == "__main__":
     # Will recreate if corrupted
     init_db()
     
-    app.run(debug=True, host="0.0.0.0", port=5000)
+   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
